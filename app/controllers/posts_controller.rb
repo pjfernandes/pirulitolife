cfe14cf7d11e059751d046a818403c43244cef4d
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-05-24 09:40:02 -0300

    navbar corrected

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index]
+
+  def index
+    @posts = Post.all.sort.reverse
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    if current_user.admin?
+      @post = Post.new
+    else
+      redirect_to posts_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:legend, :date_photo, :user_id)
+  end
+
 end

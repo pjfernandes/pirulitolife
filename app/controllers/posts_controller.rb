8faea9ff7158e1ce6cfd70f34578fad936b9d86f
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-05-24 10:00:45 -0300

    new post view created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def create
+    if current_user.admin?
+      @post = Post.new(post_params)
+      @post.user = current_user
+      if @post.save
+        redirect_to posts_path, notice: 'Post was successfully created!'
+      else
+        render :new
+      end
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:legend, :date_photo, :user_id)
